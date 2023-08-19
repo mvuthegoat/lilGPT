fafd76bb7f97a0811bcbe7f256809c45a3f10594
mvuthegoat <mvu5803@gmail.com>
2023-08-19 18:24:16 +0700

    update data pre-processing code

--- data/prepare.py
+++ data/prepare.py
@@ -1,33 +1,34 @@
 import os
 import numpy as np
-import pandas as pd
+import transformers, datasets
 
-from datasets import load_dataset
-from transformers import AutoTokenizer
 
-## dataset used: "Libosa2707/vietnamese-poem" from huggingface dataset
+def preprocess(
+    tokenizer: transformers.PreTrainedTokenizer,
+):
+    dataset = datasets.load_dataset("wiki_lingua", "vietnamese")  # wikihow texts
+    data = [d.get("document")[0] for d in dataset["train"]["article"]]
+    # Add a EOS token to the end of each example
+    data = [s + tokenizer.eos_token for s in data]
+    # Tokenize texts
+    input_ids = tokenizer(
+        data,
+    ).input_ids
 
-dataset = load_dataset("Libosa2707/vietnamese-poem")
-#print(dataset)
+    # Split train/test. Default split is 0.90
+    np.random.seed(0)
+    perm = np.random.permutation(len(input_ids))
+    split = int(len(perm) * 0.95)
+    train_indices = perm[:split]
+    eval_indices = perm[split:]
+    train_data = [input_ids[i] for i in train_indices]
+    eval_data = [input_ids[i] for i in eval_indices]
 
-dataset = dataset["train"].train_test_split(test_size=0.1)
-#dataset['validation'] = dataset['test']  # change the dict key test to valid
-#del dataset['test']
+    # Concatenate examples to make a single list of input
+    train_data = [item for example in train_data for item in example]
+    eval_data = [item for example in eval_data for item in example]
 
-## sample some dataset so that we can reduce training time.
-dataset["train"] = dataset["train"].select([i for i in range(8000)])
-dataset["validation"] = dataset["validation"].select([i for i in range(2000)])
+    train_data = np.array(train_data)
+    eval_data = np.array(eval_data)
 
-# Tokenizer
-tokenizer = AutoTokenizer.from_pretrained("distilbert-base-multilingual-cased")
-#print(tokenizer.is_fast)
-#print(tokenizer.vocab_size)
-train_data = tokenizer.encode(str(dataset["train"]["content"]))
-val_data = tokenizer.encode(str(dataset["validation"]["content"]))
-``
-train_data.tofile(os.path.join(os.path.dirname(__file__), 'train.bin'))
-val_data.tofile(os.path.join(os.path.dirname(__file__), 'val.bin'))
-
-print(f"train has {len(train_data):,} tokens")
-print(f"val has {len(val_data):,} tokens")
-print(f"The id of the word yêu in the tokenizer: {tokenizer.encode('yêu')}") # output: The id of the word yêu in the tokenizer: [101, 30162, 102]
+    return train_data, eval_data
